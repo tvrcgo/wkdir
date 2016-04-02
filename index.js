9ca9a9c11fa225858341f3ed7df710a31d7bd06c
tvrcgo <tvrcgo@gmail.com>
2016-04-02 11:50:04 +0800

    check if dir exists.

--- index.js
+++ index.js
@@ -2,16 +2,43 @@
 
 var fs = require('fs');
 
-function wkdir(dir) {
-    var dirs = dir.split('/');
+/**
+ * walk dirs
+ * @param  {string} path
+ */
+function wkdir(path) {
+    var dirs = path.split('/');
     var tar = [];
     while(dirs.length) {
         tar.push(dirs.shift());
         var t = tar.join('/');
-        if (t && !fs.accessSync(t, fs.W_OK)) {
+        if (t && !exist(t)) {
             fs.mkdirSync(t);
         }
     }
+}
+
+/**
+ * check if directory exists.
+ * @param  {string} path
+ * @return {bool}      [exists]
+ */
+function exist(path) {
+    try {
+        var stat = fs.statSync(path);
+    } catch (err) {
+        switch (err.code) {
+            case 'ENOENT':
+                return false;
+                break;
+            case 'EACCES':
+                return true;
+                break;
+            default:
+
+        }
+    }
+    return stat && stat.isDirectory();
 }
 
 module.exports = wkdir;
